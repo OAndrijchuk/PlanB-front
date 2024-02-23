22181093b623427a66d50235bcabac0aa27c8436
IrynaDidkivska <96765824+IrynaDidkivska@users.noreply.github.com>
2024-02-23 10:53:38 +0100

    feat: added up to top btn

--- src/components/ButtonScrollToUp/ButtonScrollToUp.tsx
+++ src/components/ButtonScrollToUp/ButtonScrollToUp.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+'use client';
+import { SpriteSVG } from '@/assets/img/SpriteSVG';
+import React, { useEffect, useState } from 'react';
+import { twMerge } from 'tailwind-merge';
 
-const ButtonScrollToUp = () => {
+const ButtonScrollToUp: React.FC = () => {
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      window.scrollY > 500 ? setIsVisible(true) : setIsVisible(false);
+    };
+    window.addEventListener('scroll', toggleVisibility);
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    isVisible &&
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+  };
   return (
-    <div>ButtonScrollToUp</div>
-  )
-}
+    <button
+      onClick={scrollToTop}
+      type="button"
+      className={twMerge(`fixed right-4 bottom-52 w-[36px] h-[48px]  bg-red-200 opacity-50 rounded-custom cursor-pointer hover:bg-red-100 active:bg-red-300
+      md:right-8
+      xl:right-16
+      ${isVisible ? 'visible' : 'hidden'}
+      `)}
+    >
+      <SpriteSVG name="chevron" />
+    </button>
+  );
+};
 
-export default ButtonScrollToUp
+export default ButtonScrollToUp;
