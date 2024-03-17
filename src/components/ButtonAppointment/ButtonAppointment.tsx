5ecbd8f7e18635828b9074d53471af793671d87a
IrynaDidkivska <96765824+IrynaDidkivska@users.noreply.github.com>
2024-03-17 11:40:14 +0100

    fixed: Update styles and dependencies

--- src/components/ButtonAppointment/ButtonAppointment.tsx
+++ src/components/ButtonAppointment/ButtonAppointment.tsx
@@ -1,5 +1,6 @@
 'use client';
 import React, { useState } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 type ButtonAppointmentProps = {
   type: 'submit' | 'button';
@@ -16,8 +17,8 @@ const ButtonAppointment: React.FC<ButtonAppointmentProps> = ({
       <button
         type={type}
         onClick={() => setShowModal(true)}
-        className="py-4 min-w-[18rem] text-2xl leading-6 font-medium text-center text-stone-900 bg-red-200 shadow-custom rounded-custom cursor-pointer hover:bg-red-100 active:bg-red-300
-      md:min-w-[26.25rem]"
+        className={twMerge(`py-4 min-w-[18rem] text-2xl leading-6 font-medium text-center text-stone-900 bg-red-200 shadow-custom rounded-custom cursor-pointer hover:bg-red-100 active:bg-red-300
+      md:min-w-[26.25rem]`)}
       >
         {children}
       </button>
--- src/modules/Form/helpers/handleSubmit.tsx
+++ src/modules/Form/helpers/handleSubmit.tsx
@@ -7,29 +7,4 @@ export const handleSubmit = async (
   alert(JSON.stringify(values, null, 2));
 
   action.resetForm();
-  // try {
-  //   const endpoint = '';
-
-  //   const response = await fetch(endpoint, {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify(values),
-  //   });
-
-  //   if (!response.ok) {
-  //     throw new Error('Виникла проблема з вашим запитом');
-  //   }
-
-  //   const data = await response.json();
-
-  //   console.log('Відповідь від сервера:', data);
-
-  //   alert('Форма успішно відправлена!');
-  //   alert(JSON.stringify(values, null, 2));
-  //   action.resetForm();
-  // } catch (error) {
-  //   console.error('Помилка при надсиланні запиту:', error.message);
-  // }
 };
